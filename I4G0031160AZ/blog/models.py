eddbea2c07c4e196e490e2b0497874395b5897e3
BIGBOSS <bigbosses00@gmail.com>
2022-06-19 13:06:08 +0100

    Zuri task

---
+++ I4G0031160AZ/blog/models.py
@@ -0,0 +1,15 @@
+from email.policy import default
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    texte = models.TextField()
+    author = models.ForeignKey(get_user_model(),
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(default=timezone.now)
